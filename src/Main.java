9106ed164278c98a3a6a8588c67eaf3149a0ec54
David Larsson <guslardaac@student.gu.se>
2018-03-01 20:53:46 +0100

    Changed the city contructor to be RC. Added city to main

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,22 @@ public class Main {
     static int t; // No of steps
     static int f; // No of vehicles
     static int n; // No of rides in total
+    static int r; // No of rows
+    static int c; // No of columns
 
     private static Parser parser = new Parser();
 
     public static void main(String[] args) {
     parser.parseLines();
+
     t = parser.getNumberOfSteps();
     f = parser.getNumberOfVehicles();
     n = parser.getNumberOfRides();
+    r = parser.getRows();
+    c = parser.getColumns();
+
+    City city = new City(r,c);
+
         System.out.println("Steps = " + t);
         System.out.println("Vehicles = " + f);
         System.out.println("Rides = " + n);
