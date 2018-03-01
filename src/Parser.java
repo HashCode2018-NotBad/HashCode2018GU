df2fb1c25573a30dfcf9ff02997bee2a9e09adea
guschefi <guschefi@student.gu.se>
2018-03-01 20:36:18 +0100

    Added Parser cases

--- src/Parser.java
+++ src/Parser.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class Parser {
 
     private static City city;
+    private static Car[] cars;
     private static int rows = 0;
     private static int columns = 0;
     private static int numberOfVehicles = 0;
+    private static int numberOfRides = 0;
+    private static int bonus = 0;
+    private static int numberOfSteps = 0;
 
     public Parser() {
 
     }
 
-    public static void main(String[] args) {
+    public void parseLines() {
 
         String fileName = "./a_example.in";
         String s = "";
@@ -29,22 +33,47 @@ public class Parser {
                         counter = 0;
                         i++;
                     }
-                    if (i == 1) {
+                    else if (i == 1) {
                         while (s.charAt(counter) != (' ')) {
                             counter++;
                         }
-                        columns = Integer.parseInt(s.substring(0, counter));
+                        columns = Integer.parseInt(s.substring(0, counter + 1));
                         counter = 0;
                         i++;
                     }
-                    if (i == 2) {
+                    else if (i == 2) {
                         while (s.charAt(counter) != (' ')) {
                             counter++;
                         }
-                        numberOfVehicles = Integer.parseInt(s.substring(0, counter));
+                        numberOfVehicles = Integer.parseInt(s.substring(0, counter + 1));
                         counter = 0;
                         i++;
                     }
+                    else if (i == 3) {
+                        while (s.charAt(counter) != (' ')) {
+                            counter++;
+                        }
+                        numberOfRides = Integer.parseInt(s.substring(0, counter + 1));
+                        counter = 0;
+                        i++;
+                    }
+                    else if (i == 4) {
+                        while (s.charAt(counter) != (' ')) {
+                            counter++;
+                        }
+                        bonus = Integer.parseInt(s.substring(0, counter + 1));
+                        counter = 0;
+                        i++;
+                    }
+                    else if (i == 5) {
+                        while (s.charAt(counter) != (' ')) {
+                            counter++;
+                        }
+                        numberOfSteps = Integer.parseInt(s.substring(0, counter + 1));
+                        counter = 0;
+                        i++;
+                    }
+
                 }
             }
         } catch (IOException e) {
