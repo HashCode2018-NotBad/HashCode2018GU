b3468346787b9e0cadcee3e52135081cd8232eab
Noahnom <gusfawom@student.gu.se>
2018-03-01 20:47:34 +0100

    removed firstLine out of loop

--- src/Parser.java
+++ src/Parser.java
@@ -18,69 +18,64 @@ public class Parser {
     }
 
     public void parseLines() {
-
         String fileName = "./a_example.in";
         String s = "";
         int counter = 0;
         try (Scanner scanner = new Scanner(new File(fileName))) {
-            while (scanner.hasNext()){
-                for (int i = 0; i < 6;) {
-                    if (i == 0) {
-                        while (s.charAt(counter) != (' ')) {
-                            counter++;
-                        }
-                        rows = Integer.parseInt(s.substring(0, counter + 1));
-                        counter = 0;
-                        i++;
+            String firstLine = scanner.nextLine();
+            for (int i = 0; i < 6;) {
+                if (i == 0) {
+                    while (firstLine.charAt(counter) != (' ')) {
+                        counter++;
                     }
-                    else if (i == 1) {
-                        while (s.charAt(counter) != (' ')) {
-                            counter++;
-                        }
-                        columns = Integer.parseInt(s.substring(0, counter + 1));
-                        counter = 0;
-                        i++;
+                    rows = Integer.parseInt(firstLine.substring(0, counter));
+                    counter = 0;
+                    i++;
+                }
+                else if (i == 1) {
+                    while (firstLine.charAt(counter) != (' ')) {
+                        counter++;
                     }
-                    else if (i == 2) {
-                        while (s.charAt(counter) != (' ')) {
-                            counter++;
-                        }
-                        numberOfVehicles = Integer.parseInt(s.substring(0, counter + 1));
-                        counter = 0;
-                        i++;
+                    columns = Integer.parseInt(firstLine.substring(0, counter));
+                    counter = 0;
+                    i++;
+                }
+                else if (i == 2) {
+                    while (firstLine.charAt(counter) != (' ')) {
+                        counter++;
                     }
-                    else if (i == 3) {
-                        while (s.charAt(counter) != (' ')) {
-                            counter++;
-                        }
-                        numberOfRides = Integer.parseInt(s.substring(0, counter + 1));
-                        counter = 0;
-                        i++;
+                    numberOfVehicles = Integer.parseInt(firstLine.substring(0, counter));
+                    counter = 0;
+                    i++;
+                }
+                else if (i == 3) {
+                    while (firstLine.charAt(counter) != (' ')) {
+                        counter++;
                     }
-                    else if (i == 4) {
-                        while (s.charAt(counter) != (' ')) {
-                            counter++;
-                        }
-                        bonus = Integer.parseInt(s.substring(0, counter + 1));
-                        counter = 0;
-                        i++;
+                    numberOfRides = Integer.parseInt(firstLine.substring(0, counter));
+                    counter = 0;
+                    i++;
+                }
+                else if (i == 4) {
+                    while (firstLine.charAt(counter) != (' ')) {
+                        counter++;
                     }
-                    else if (i == 5) {
-                        while (s.charAt(counter) != (' ')) {
-                            counter++;
-                        }
-                        numberOfSteps = Integer.parseInt(s.substring(0, counter + 1));
-                        counter = 0;
-                        i++;
+                    bonus = Integer.parseInt(firstLine.substring(0, counter));
+                    counter = 0;
+                    i++;
+                }
+                else if (i == 5) {
+                    while (firstLine.charAt(counter) != (' ')) {
+                        counter++;
                     }
-
+                    numberOfSteps = Integer.parseInt(firstLine.substring(0, counter));
+                    counter = 0;
+                    i++;
                 }
             }
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
-
 }
