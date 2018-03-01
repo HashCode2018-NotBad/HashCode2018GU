3327235eec06b66f7a3a70db4de84e0f91fbd267
David Larsson <guslardaac@student.gu.se>
2018-03-01 20:48:19 +0100

    Made parser in main, test functions

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
 
-    int t; // No of steps
-    int f; // No of vehicles
-    int n; // No of rides in total
+    static int t; // No of steps
+    static int f; // No of vehicles
+    static int n; // No of rides in total
 
+    private static Parser parser = new Parser();
 
     public static void main(String[] args) {
-
+    parser.parseLines();
+    t = parser.getNumberOfSteps();
+    f = parser.getNumberOfVehicles();
+    n = parser.getNumberOfRides();
+        System.out.println("Steps = " + t);
+        System.out.println("Vehicles = " + f);
+        System.out.println("Rides = " + n);
     }
 }
